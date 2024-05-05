e904680a9c2644beb48a23b1d1fa22a1c47e3bb1
Eventtime2024 <168448599+Eventtime2024@users.noreply.github.com>
2024-05-05 20:32:13 +0300

    Add files via upload

---
+++ css/script3.js
@@ -0,0 +1,69 @@
+$(window).on( 'load', function() {
+    $('html, body').animate({scrollTop : 0});
+    });
+
+    let date=new Date("July 15 2024 00:00:00");
+function counts(){
+    let now= new Date();
+    gap=date - now;
+
+  let days =Math.floor(gap/1000/60/60/24)
+  let hours =Math.floor(gap/1000/60/60)%24;
+  let minutes=Math.floor(gap/1000/60)%60;
+  let seconds=Math.floor(gap/1000) % 60;
+  let formatted_days = days.toString().padStart(2, '0');
+  let formatted_hours = hours.toString().padStart(2, '0');
+  let formatted_minutes = minutes.toString().padStart(2, '0');
+  let formatted_seconds = seconds.toString().padStart(2, '0');
+ $("#d").text(formatted_days)
+ $("#h").text(formatted_hours) ;
+  $("#m").text(formatted_minutes);
+  $("#s").text(formatted_seconds);
+  if(gap<0){
+    $("#tillwedding").html("<p>ДЕНЬ СВАДБЫ"+"<br>"+"16.07.2024</p>")
+    $("#group").text(" ")
+  }
+  if(days<2){
+   $(".lastpart").css("display","none")
+  }
+
+}
+counts();
+setInterval(counts,1000);
+
+
+let audio=document.getElementById("myAudio")
+ $("#play").click(function(){
+  if(($("#play").css("width")) == "50px"){
+      $("#play").css("width", "30px");
+      $("#play").css("height", "30px");
+      audio.pause();
+      audio.currentTime = 0;
+  } 
+  else {
+      $("#play").css("width","50px");
+      $("#play").css("height","50px");
+      audio.play();
+  }
+});
+
+// $(document).on("contextmenu", function(e) {
+//   e.preventDefault();
+// });
+
+
+function dropHandler(event) {
+  event.preventDefault();
+
+  const url = event.dataTransfer.getData('text/plain');
+  const img = document.createElement('img');
+  img.src = url("../img/Group 13.png");
+  img.onload = function() {
+      document.getElementById('dropzone').innerHTML = '';
+      document.getElementById('dropzone').appendChild(img);
+  };
+}
+
+function dragOverHandler(event) {
+  event.preventDefault();
+}
