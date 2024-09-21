2b62ee580740fd15a581da1ddb23481063bee731
Eventtime2024 <168448599+Eventtime2024@users.noreply.github.com>
2024-09-21 21:14:00 +0300

    Add files via upload

---
+++ js/script6.js
@@ -0,0 +1,28 @@
+let date=new Date("November 22 2024 00:00:00");
+function counts(){
+    let now= new Date();
+    gap=date - now;
+
+  let days =Math.floor(gap/1000/60/60/24)
+  let hours =Math.floor(gap/1000/60/60)%24;
+  let minutes=Math.floor(gap/1000/60)%60;
+  let seconds=Math.floor(gap/1000) % 60;
+  let formatted_days = days.toString().padStart(2, '0');
+  let formatted_hours = hours.toString().padStart(2, '0');
+  let formatted_minutes = minutes.toString().padStart(2, '0');
+  let formatted_seconds = seconds.toString().padStart(2, '0');
+ $("#d").text(formatted_days)
+ $("#h").text(formatted_hours) ;
+  $("#m").text(formatted_minutes);
+  $("#s").text(formatted_seconds);
+  if(gap<0){
+    $("#tillwedding").html("<p>ДЕНЬ СВАДБЫ"+"<br>"+"22 November 2024</p>")
+    $("#group").text(" ")
+  }
+  if(days<2){
+   $(".lastpart").css("display","none")
+  }
+
+}
+counts();
+setInterval(counts,1000);
